e70c9601de040598053989f9b5f819e9b72d3f19
avirup-ghosal <avirupghosal2023@gmail.com>
2025-08-20 21:15:29 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+
 
 
 dotenv.config();
@@ -15,6 +17,7 @@ app.use(express.json());
 
 
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
